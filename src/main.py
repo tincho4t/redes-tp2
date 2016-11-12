063f9afd607118b2e66a2eb96dd0ae884d026942
tincho4t <martinforte@yahoo.com.ar>
2016-11-12 11:58:05 -0300

    Borro un host de clarin y aumento el TTL

--- src/main.py
+++ src/main.py
@@ -1,12 +1,11 @@
 from Tracer import Tracer
 
-hostname = "sydney.edu.au"
+# hostname = "sydney.edu.au"
 #hostname = "www.google.com"
-#hostname = "www.u-tokyo.ac.jp"
-#hostname = "www.clarin.com" # Clarin no responde el trace pero si el sync
-# hostname = "www.indian-host.com" # Clarin no responde el trace pero si el sync
+hostname = "www.u-tokyo.ac.jp"
+# hostname = "www.indian-host.com"
 # hostname = "www.bandainamco.co.jp"
 
 t = Tracer(timeout=5)
 #t.traceEachStepRoute(hostname, 36, 5)
-t.traceEachRoute(hostname, 36, timesForTtl=1)
+t.traceEachRoute(hostname, 36, timesForTtl=100)
